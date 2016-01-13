7662adfbdc5df3d0d40ae888554b6a01c446de8f
Thomas A Caswell <tcaswell@gmail.com>
2016-01-12 23:17:26 -0500

    start to add my stuff

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,6 @@
 alias reload!='. ~/.zshrc'
+
+alias ls='ls --color'
+alias conda_on='export PATH=/home/tcaswell/mc3/bin:$PATH'
+eval "$(hub alias -s)"
+export GITHUB_USER=tacaswell
--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,5 @@
 #add each topic folder to fpath so that they can add functions and completion scripts
 for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+
+export PATH=$PATH:~/local_installs/bin/:~/bin
+export PATH=/usr/lib64/ccache/bin:$PATH
